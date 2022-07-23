fdd88993d4f8f1192134158509b0685dc089f7b6
Subashreebalakrishnan <107244820+Subashreebalakrishnan@users.noreply.github.com>
2022-07-23 17:57:53 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,24 @@
+let counterElement = document.querySelector(".counter");
+
+const promiseCheck = new Promise((resolve, reject) => {
+  let Timer = setInterval(() => {
+    var runner = parseInt(document.querySelector(".counter").innerText);
+    counterElement.innerText = runner - 1;
+    if (runner === 0) {
+     clearInterval(Timer);
+     resolve((counterElement.innerText = "Happy Independence Day"));
+    }else{
+      reject("wht going wrong")
+    }
+    // console.log(runner)
+  }, 1000);
+});
+console.log(promiseCheck);
+
+
+promiseCheck
+  .then((data) => console.log(data))
+  .catch((errData) => console.log(errData));
+
+
+   
